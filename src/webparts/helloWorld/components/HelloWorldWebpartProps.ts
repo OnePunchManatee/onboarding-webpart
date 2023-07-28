325979bad2afc11b310bb0e1ba6b91c0a13029b7
Noah Davison <ndavison@normal.org>
2023-07-28 12:26:13 -0500

    current build

---
+++ src/webparts/helloWorld/components/HelloWorldWebpartProps.ts
@@ -0,0 +1,13 @@
+// import {
+//     IReadonlyTheme,
+// }from "@microsoft/sp-component-base";
+// import { WebPartContext } from "@microsoft/sp-webpart-base";
+export interface HelloWorldWebpartProps {
+    documentTitle: string;
+    currentUserDisplayName: string;
+    storageList: string;
+    acknowledgementLabel: string;
+    acknowledgementMessage: string;
+    readMessage: string;
+
+}
